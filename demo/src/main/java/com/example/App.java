61a93ce66e6a47d07e6af03af0ea2ac2d3bdb092
neojarvis <techadmin@iamneo.ai>
2025-04-17 07:34:28 +0000

    Manual-save-commit-17/04/2025--13:04:27

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class App {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
@@ -45,6 +46,9 @@ public class App {
         System.out.println(username);
         // driver.quit();
 
+
+        // Select select = new Select(driver.findElement(By.xpath("")));
+        
         driver.close();
 
     }
