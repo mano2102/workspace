903785b3eb4275a3f1853e6b81dedff1353272c8
neojarvis <techadmin@iamneo.ai>
2025-04-17 07:10:58 +0000

    Auto-save-commit-17/04/2025--12:40:56

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,19 +1,36 @@
 package com.example;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws MalformedURLException
     {
-        System.out.println( "Hello World!" );
-        WebDriver driver = new ChromeDriver();
-
+      String url="http://localhost:4444";
+        WebDriver driver = new RemoteWebDriver(new URL(url),new ChromeOptions());
+        // maximize
+        driver.manage().window().maximize();
         // getting the url
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
-
-        // driver.close();
+        // username 
+        WebElement username=driver.findElement(By.name("username"));
+        username.click();
+        username.sendKeys("Admin");
+        // password
+        WebElement password =driver.findElement(By.xpath("//input[@name='password'] ")) ;
+        password.click();
+        password.sendKeys("admin123");
+        // login click
+        WebElement login = driver.findElement(By.xpath(""));
+        login.click();
     }
 }
