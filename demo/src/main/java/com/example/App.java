536cebc1c20d244937745fc13be1cf6ef23aa17e
neojarvis <techadmin@iamneo.ai>
2025-04-17 07:25:57 +0000

    Auto-save-commit-17/04/2025--12:55:56

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -10,27 +10,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class App 
-{
-    public static void main( String[] args ) throws MalformedURLException
-    {
-      String url="http://localhost:4444";
-        WebDriver driver = new RemoteWebDriver(new URL(url),new ChromeOptions());
+public class App {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+        String gridurl = "http://localhost:4444";
+        WebDriver driver = new RemoteWebDriver(new URL(gridurl), new ChromeOptions());
         // maximize
         driver.manage().window().maximize();
         // getting the url
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        String currentURL = driver.getCurrentUrl();
+        Thread.sleep(4000);
 
-        // username 
-        WebElement username=driver.findElement(By.name("username"));
-        username.click();
+        // username
+        WebElement username = driver.findElement(By.xpath("//input[@name='username'] "));
+        // username.click();
         username.sendKeys("Admin");
         // password
-        WebElement password =driver.findElement(By.xpath("//input[@name='password'] ")) ;
-        password.click();
+        WebElement password = driver.findElement(By.xpath("//input[@name='password'] "));
+        // password.click();
         password.sendKeys("admin123");
         // login click
-        WebElement login = driver.findElement(By.xpath(""));
+        WebElement login = driver.findElement(By.xpath("//button[@type='submit'] "));
         login.click();
+        String actualURL = driver.getCurrentUrl();
+
+        if (actualURL.equals(currentURL)) {
+            System.out.println("Login Failed");
+
+        } else {
+            System.out.println("Login Success");
+
+        }
+
+        System.out.println(username);
+        // driver.quit();
+
+        driver.close();
+
     }
 }
