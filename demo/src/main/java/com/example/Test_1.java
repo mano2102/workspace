92a588e993d97052882792fd1a529a289cfa382f
neojarvis <techadmin@iamneo.ai>
2025-04-18 06:28:39 +0000

    Auto-save-commit-18/04/2025--11:58:37

--- demo/src/main/java/com/example/Test_1.java
+++ demo/src/main/java/com/example/Test_1.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class Test_1 {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
         String gridurl = "http://localhost:4444";
         String pageURL = "https://demoqa.com/automation-practice-form";
         WebDriver driver = new RemoteWebDriver(new URL(gridurl), new ChromeOptions());
@@ -22,11 +22,13 @@ public class Test_1 {
         // getting the url
         driver.get(pageURL);
         // page load time
-
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 
+        // implicit wait for 10 seconds
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         // enter the first name
-        WebElement firstName = driver.findElement(By.id(""));
+        WebElement firstName = driver.findElement(By.id("firstName"));
         firstName.sendKeys("mano");
 
         // enter the last name
@@ -37,27 +39,31 @@ public class Test_1 {
         WebElement userEmail = driver.findElement(By.id("userEmail"));
         userEmail.sendKeys("mano@gmail.com");
 
-        // enter the last name
-        WebElement maleCheckbox = driver.findElement(By.id("Male"));
+        // // enter the gender name
+        WebElement maleCheckbox = driver.findElement(By.xpath("//label[@for='gender-radio-1']"));
         maleCheckbox.click();
         WebElement mobileNumber = driver.findElement(By.id("userNumber"));
         mobileNumber.sendKeys("9898989898");
 
         WebElement dateOfBirth = driver.findElement(By.id("dateOfBirthInput"));
         dateOfBirth.click();
-      
+
         // select month
-        WebElement monthSelect=driver.findElement(By.xpath("//select[@class='react-datepicker__month-select']"));
-        Select selectMonth= new Select(monthSelect);
+        WebElement monthSelect = driver.findElement(By.xpath("//select[@class='react-datepicker__month-select']"));
+        Select selectMonth = new Select(monthSelect);
         selectMonth.selectByValue("6");
 
         // select year
 
-        WebElement yearSelect=driver.findElement(By.xpath("//select[@class='react-datepicker__year-select']"));
-        Select selectYear= new Select(yearSelect);
+        WebElement yearSelect = driver.findElement(By.xpath("//select[@class='react-datepicker__year-select']"));
+        Select selectYear = new Select(yearSelect);
         selectYear.selectByValue("2025");
         // selecting the date
+        WebElement dateClick = driver
+                .findElement(By.xpath("//div[@class='react-datepicker__day react-datepicker__day--023']"));
 
+        dateClick.click();
+        Thread.sleep(3000);
         // closing or quitting the driver
         if (driver != null) {
             driver.quit();
