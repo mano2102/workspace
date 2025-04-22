cbed2aae9def52364187598fca3f53b52bfd383b
neojarvis <techadmin@iamneo.ai>
2025-04-22 06:38:03 +0000

    Auto-save-commit-22/04/2025--12:08:02

---
+++ demo/src/main/java/com/example/ExcelReader.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.io.FileInputStream;
+
+public class ExcelReader {
+
+    public static void main(String[] args) {
+     System.out.println("Reading excel");
+
+
+     String filePath="/home/coder/project/workspace/demo/testdata/login.xlsx";
+
+     FileInputStream fis = new FileInputStream();
+
+    }
+}
--- demo/src/test/java/com/example/Hybrid.java
+++ demo/src/test/java/com/example/Hybrid.java
@@ -9,10 +9,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 public class Hybrid {
 
@@ -50,5 +52,12 @@ public class Hybrid {
         driver.switchTo().parentFrame();
         WebElement outsideFrame1Page = driver.findElement(By.xpath(outsideFrame1));
         System.out.println(outsideFrame1Page.getText());
+        Assert.assertEquals("mano", "mano");
     }
+
+    @Test
+    public void login_demoPage() {
+        driver.get("");
+    }
+
 }
