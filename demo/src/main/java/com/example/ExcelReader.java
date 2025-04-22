4acdc47c1f340d60cb4ae6a301327853ecf173a6
neojarvis <techadmin@iamneo.ai>
2025-04-22 06:53:03 +0000

    Auto-save-commit-22/04/2025--12:23:02

--- demo/src/main/java/com/example/ExcelReader.java
+++ demo/src/main/java/com/example/ExcelReader.java
@@ -1,16 +1,30 @@
 package com.example;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelReader {
 
-    public static void main(String[] args) {
-     System.out.println("Reading excel");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Reading excel");
 
+        String filePath = "/home/coder/project/workspace/demo/testdata/login.xlsx";
 
-     String filePath="/home/coder/project/workspace/demo/testdata/login.xlsx";
+        FileInputStream fis = new FileInputStream(new File(filePath));
 
-     FileInputStream fis = new FileInputStream();
-
+        XSSFWorkbook workBook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workBook.getSheet("Sheet1");
+        XSSFRow row = sheet.getRow(5);
+        XSSFCell usernameCell = row.getCell(0);
+        XSSFCell passwordCell = row.getCell(1);
+        // System.out.println(passwordCell);
+        System.out.println(usernameCell.getStringCellValue());
+        System.out.println(passwordCell.getStringCellValue());
     }
 }
