645d6f656ba2f72f551b1bbdc495417fcda7f798
neojarvis <techadmin@iamneo.ai>
2025-04-23 07:04:45 +0000

    Auto-save-commit-23/04/2025--12:34:43

--- demo/src/test/java/com/example/pages/LoginPagePages.java
+++ demo/src/test/java/com/example/pages/LoginPagePages.java
@@ -1,5 +1,7 @@
 package com.example.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -11,6 +13,7 @@ public class LoginPagePages {
     BookStoreLoginUI loginUiLocators = new BookStoreLoginUI();
 
     private WebDriver driver = null;
+    Logger logger = LogManager.getLogger(LoginPagePages.class);
 
     public LoginPagePages(WebDriver driver) {
 
@@ -20,18 +23,24 @@ public class LoginPagePages {
 
     public void enter_username(String username) {
         WebElement usernameBox = WebDriverHelper.getElementByXpath(loginUiLocators.usernameXpath);
+        logger.info("Username box is identified");
         usernameBox.sendKeys(username);
+        logger.info("Username  is entered.");
 
     }
 
     public void enter_password(String password) {
         WebElement passwordBox = driver.findElement(loginUiLocators.passwordXpath);
+        logger.info("Password box is identified");
         passwordBox.sendKeys(password);
+        logger.info("Password  is entered.");
     }
 
     public void login_button() {
         WebElement loginButton = driver.findElement(loginUiLocators.loginButtonXpath);
+        logger.info("Login button is identified");
         loginButton.click();
+        logger.info("Login button is clicked.");
     }
 
 }
